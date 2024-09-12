a2ebbaa73759dbc892988ebca8a082a9402ced36
Taisuke Fukuno <fukuno@jig.jp>
2024-09-12 09:30:04 +0900

    remove support * encoding

--- encodeURIComponent2.js
+++ encodeURIComponent2.js
@@ -1,14 +1,6 @@
-const encs = "()*";
-
-const fix02 = (s) => s.length == 1 ? "0" + s : s;
-
 export const encodeURIComponent2 = (s) => {
-  if (!s) return s;
   s = encodeURIComponent(s);
-  for (const c of encs) {
-    const ch = "%" + fix02(c.charCodeAt(0).toString(16));
-    s = s.replace(new RegExp("\\" + c, "g"), ch);
-    console.log(c, ch);
-  }
+  s = s.replace(/\(/g, "%28");
+  s = s.replace(/\)/g, "%29");
   return s;
 };
