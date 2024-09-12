a6ac3dc718736b3a000179842a7fb0376f6cce9a
Taisuke Fukuno <fukuno@jig.jp>
2024-09-12 09:23:54 +0900

    add support encoding '*'

--- encodeURIComponent2.js
+++ encodeURIComponent2.js
@@ -1,6 +1,14 @@
+const encs = "()*";
+
+const fix02 = (s) => s.length == 1 ? "0" + s : s;
+
 export const encodeURIComponent2 = (s) => {
+  if (!s) return s;
   s = encodeURIComponent(s);
-  s = s.replace(/\(/g, "%28");
-  s = s.replace(/\)/g, "%29");
+  for (const c of encs) {
+    const ch = "%" + fix02(c.charCodeAt(0).toString(16));
+    s = s.replace(new RegExp("\\" + c, "g"), ch);
+    console.log(c, ch);
+  }
   return s;
 };
